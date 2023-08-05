db3d19a3d82abf58009b30ce762f1696db53015f
Bartlomiej Swies <bartlomiejswies@gmail.com>
2023-08-05 20:31:52 +0200

    show and add restaurants and menus

--- js/ownerRestaurants.js
+++ js/ownerRestaurants.js
@@ -1,20 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let basicUrl = 'http://localhost:8080';
 
     $.get({
         // TODO: to replace 1 with owner data
         url: basicUrl + '/food-app/restaurants/owner/1'
     }).done(function (data) {
-        console.log(data);
         generateRestaurantList(data);
 
     }).fail(function () {
         alert("cos poszlo nie tak");
     });
 
-    $("#addRestaurant").on("click", function () {
-        // TODO: to replace 1 with owner data
-        window.location = 'newRestaurant.html?userId=1'
+    $("#newRestaurantForm").on('submit', function (e) {
+        e.preventDefault();
+
+        var dataFromForm = $(this).serialize().split("&");
+        var dataToSend = {};
+        for (var key in dataFromForm) {
+            dataToSend[dataFromForm[key].split("=")[0]] = dataFromForm[key].split("=")[1];
+        }
+
+        console.log(dataToSend);
+        console.log(JSON.stringify(dataToSend));
+
+        $.ajax({
+            type: $(this).prop('method'),
+            url: $(this).prop('action'),
+            contentType: "application/json",
+            data: JSON.stringify(dataToSend)
+        }).done(function () {
+            alert("Restauracja została dodana!");
+            window.location.reload();
+        });
     });
 });
 
@@ -23,9 +40,9 @@ generateRestaurantList = function (data) {
     for (let i = 0; i < data.length; i++) {
         htmlToInsert += '<li class="list-group-item">' +
             '<div class="d-flex justify-content-between">' +
-            '<span class="w-25">' + data[i].name +'</span>' +
+            '<span class="w-25">' + data[i].name + '</span>' +
 
-            '<a href="restaurantMenuList.html?restaurantId=' + data[i].restaurantId + '">' +
+            '<a href="ownerRestaurantMenuList.html?restaurantId=' + data[i].restaurantId + '">' +
             '<button type="button" class="btn btn-primary" value="' + data[i].restaurantId + '">Wyświetl Menu</button>' +
             '</a>' +
 
