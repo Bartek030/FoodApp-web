c82f8e553b8facca5c973b4e42f57b41f1b604b2
Bartlomiej Swies <bartlomiejswies@gmail.com>
2023-08-06 17:55:12 +0200

    Owners menu action

--- js/ownerMenu.js
+++ js/ownerMenu.js
@@ -44,15 +44,55 @@ $(document).ready(function () {
 generateFoodList = function (data) {
     let htmlToInsert = '';
     for (let i = 0; i < data.length; i++) {
-        foodNumber = data[i].foodId;
         htmlToInsert += '<div class="card mx-3" style="width: 18rem;">' +
-            '<img src="resources/img/logo.jpg" class="card-img-top" alt="...">' +
+            '<img id="foodImage' + data[i].foodId + '" class="card-img-top h-75 mx-auto" alt="Nie znaleziono obrazka">' +
             '<div class="card-body text-center">' +
             '<h5 class="card-title">' + data[i].name + ' - ' + data[i].price + ' zł/szt</h5>' +
             '<p class="card-text">' + data[i].description + '</p>' +
             '<input type="hidden" id="foodName' + data[i].foodId + '" value="' + data[i].name + '"></input>' +
+            '<button type="button" class="btn btn-primary addPictureButton" value="' + data[i].foodId + '"' +
+            'data-bs-toggle="modal" data-bs-target="#pictureModal"' +
+            '>Dodaj/Zmień zdjęcie</button>' +
             '</div>' +
             '</div>';
+        getImageFromBackend(data[i].foodId);
     }
     $(".mainContext").html(htmlToInsert);
+
+    handleAddPictureButton();
+};
+
+getImageFromBackend = function (foodId) {
+
+    let urlAddress = basicUrl + '/food-app/image/' + foodId;
+
+    fetch(urlAddress)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.blob();
+        })
+        .then(blob => {
+            const objectURL = URL.createObjectURL(blob);
+            $("#foodImage" + foodId).attr("src", objectURL);
+        })
+        .catch(error => console.error('Wystąpił błąd:', error));
 }
+
+handleAddPictureButton = function () {
+    $(".addPictureButton").on("click", function () {
+        let foodIdValue = $(this).val();
+        $("#addPictureForm").on('submit', function (e) {
+            e.preventDefault();
+
+            const photo = document.getElementById("fileInput").files[0];
+            const formData = new FormData();
+
+            formData.append("file", photo);
+            fetch(basicUrl + '/food-app/image/new/' + foodIdValue, {method: "POST", body: formData});
+            alert("Obrazek został dodany!");
+            window.location.reload();
+        });
+    });
+};
